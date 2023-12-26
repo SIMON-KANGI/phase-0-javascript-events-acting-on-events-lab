59327133d0b13feed834f57a4b45897fc88c71b1
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-26 15:22:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+// Your code here 
+//moveDodgerLeft
+let dodger=document.getElementById('dodger')
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+dodger.addEventListener('click', moveDodgerLeft)
+//moveDodgerRight()
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left  +1}px`;
+    }
+  }
+  dodger.addEventListener('mouseover', moveDodgerRight)
